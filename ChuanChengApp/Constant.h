5de581f659e94ac4968b5f0c42846f214e318938
liaoyu <liaoyu@liaoyudeimac.local>
2018-12-19 20:28:30 +0800

    好像有遗漏

---
+++ ChuanChengApp/Constant.h
@@ -0,0 +1,9 @@
+#define UISCreen_Width [UIScreen mainScreen].bounds.size.width
+#define UISCreen_Height [UIScreen mainScreen].bounds.size.height
+#define StatusBarHeight UIApplication.sharedApplication.statusBarFrame.size.height
+#define TabBarHeight UIApplication.sharedApplication.statusBarFrame.size.height >= 44?150.00:100.00
+#define IS_IPHONE_X UIApplication.sharedApplication.statusBarFrame.size.height >= 44
+
+
+//测试地址
+static const NSString *SERVSER_URL=@"http://39.108.115.189:8001";
